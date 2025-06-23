dfd20a01ac9ea81a64ec0ec0d819990b3561ac4c
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-06-23 14:33:16 -0300

    Ajustando CORS

--- src/main/java/com/gume/mapa_dinamico_motorlub/config/CorsConfig.java
+++ src/main/java/com/gume/mapa_dinamico_motorlub/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") 
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*");
             }
